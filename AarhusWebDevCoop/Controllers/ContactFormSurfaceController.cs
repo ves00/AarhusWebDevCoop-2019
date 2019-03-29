4faf50c2da2575019d3d81af66c3f5130b018f8b
Veselin Veselinov <43546748+ves00@users.noreply.github.com>
2019-03-29 10:52:08 +0100

    Initial Commit

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+
+            MailMessage message = new MailMessage();
+            message.To.Add("studentstudent082@gmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+
+
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network; smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("studentstudent082@gmail.com", "password");
+
+                //send mail 
+                smtp.Send(message);
+
+
+
+
+                TempData["success"] = true;
+            }
+
+            IContent msg = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "message");
+
+            msg.SetValue("messageName", model.Name);
+            msg.SetValue("email", model.Email);
+            msg.SetValue("subject", model.Subject);
+            msg.SetValue("messageContent", model.Message);
+
+            //Save
+            Services.ContentService.Save(msg);
+
+            return RedirectToCurrentUmbracoPage();
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
